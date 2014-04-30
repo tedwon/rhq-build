33d7e21de6f066197d7bb9d55d248b15a9dc811c
Garik Khachikyan <gkhachik@redhat.com>
2014-04-30 17:06:22 +0200

    new logic using shell scripts for both: build and compile

---
+++ rhq-build-docker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export RHQ_VERSION=4.11.0-SNAPSHOT
+export MAVEN_MAJOR=3
+export MAVEN_MINOR=2.1
+if [[ $(cat /etc/redhat-release | grep Fedora) ]]; then
+  export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
+else
+  export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
+fi
+export MAVEN_HOME=/opt/apache-maven-3.2.1
+export PATH=${MAVEN_HOME}/bin:${JAVA_HOME}/bin:${PATH};
+
+# install packages needed
+yum -y install tar git wget unzip openssh-clients java-1.7.0-openjdk-devel postgresql-server
+
+# maven install
+wget -q http://www.us.apache.org/dist/maven/maven-${MAVEN_MAJOR}/${MAVEN_MAJOR}.${MAVEN_MINOR}/binaries/apache-maven-${MAVEN_MAJOR}.${MAVEN_MINOR}-bin.tar.gz -O /opt/apache-maven-${MAVEN_MAJOR}.${MAVEN_MINOR}-bin.tar.gz 2>&1 >/dev/null
+tar xzvf /opt/apache-maven-${MAVEN_MAJOR}.${MAVEN_MINOR}-bin.tar.gz -C /opt 2>&1 >/dev/null
+mkdir /root/.m2/
+curl -sSL https://raw.githubusercontent.com/gkhachik/rhq-build/master/settings.xml > /root/.m2/settings.xml
+sed -i "s/<rhq.rootDir>.*<\/rhq.rootDir>/<rhq.rootDir>\/root\/rhq\/rhq-build<\/rhq.rootDir>/g" /root/.m2/settings.xml
+
+# postgres configuration
+su -l postgres -c "/usr/bin/initdb --pgdata='/var/lib/pgsql/data' --auth='ident'" \  >> "/var/lib/pgsql/initdb.log" 2>&1 < /dev/null
+sed -i 's/ident/trust/'  /var/lib/pgsql/data/pg_hba.conf
+su -l postgres -c " pg_ctl -l server.log -w stop; pg_ctl -l server.log -w start; ";
+createuser -h 127.0.0.1 -p 5432 -U postgres -S -D -R -w rhqadmin;
+createdb -h 127.0.0.1 -p 5432 -U postgres -O rhqadmin rhq;
+createdb -h 127.0.0.1 -p 5432 -U postgres -O rhqadmin rhqdev
+
+# git clone the repo
+mkdir -p /root/rhq/rhq-build/
+git clone https://github.com/rhq-project/rhq.git /root/rhq/rhq-build
+cd /root/rhq/rhq-build
+
+# run the mvn build
+mvn -Penterprise,dev -Ddbsetup -DskipTests install
+
---
+++ rhq-push.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+export RHQ_VERSION=4.11.0-SNAPSHOT
+export MAVEN_MAJOR=3
+export MAVEN_MINOR=2.1
+export RHQ_BRANCH=master
+if [[ $(cat /etc/redhat-release | grep Fedora) ]]; then
+  export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
+else
+  export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
+fi
+export MAVEN_HOME=/opt/apache-maven-3.2.1
+export PATH=${MAVEN_HOME}/bin:${JAVA_HOME}/bin:${PATH};
+
+cd /root/rhq/rhq-build
+git pull origin ${RHQ_BRANCH}
+
+# drop DB and init it again
+su -l postgres -c " pg_ctl -l server.log -w stop; pg_ctl -l server.log -w start; "
+dropdb -h 127.0.0.1 -p 5432 -U postgres rhqdev
+dropdb -h 127.0.0.1 -p 5432 -U postgres rhq
+dropuser -h 127.0.0.1 -p 5432 -U postgres rhqadmin
+createuser -h 127.0.0.1 -p 5432 -U postgres -S -D -R -w rhqadmin;
+createdb -h 127.0.0.1 -p 5432 -U postgres -O rhqadmin rhq;
+createdb -h 127.0.0.1 -p 5432 -U postgres -O rhqadmin rhqdev
+
+# run the mvn build
+mvn -Penterprise,dev -Ddbsetup -DskipTests install
+
+# prepare to upload to sourceforge.com
+rm -f /root/.ssh/id_dsa_rhq_sourceforge*
+mkdir /root/.ssh
+echo -e "\n ** ** SSH_PRIVATE_KEY=[$SSH_PRIVATE_KEY]"
+curl -sSL ${SSH_PRIVATE_KEY} > /root/.ssh/id_dsa_rhq_sourceforge
+chmod 600 /root/.ssh/id_dsa_rhq_sourceforge
+# scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /root/.ssh/id_dsa_rhq_sourceforge -v /root/rhq/rhq-build/modules/enterprise/server/appserver/target/rhq-server-${RHQ_VERSION}.zip -t gkhachik,rhqbuild@shell.sourceforge.net:/home/frs/project/rhqbuild/rhq/rhq-${RHQ_VERSION}/
+
+rsync -aiv -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /root/.ssh/id_dsa_rhq_sourceforge" /root/rhq/rhq-build/modules/enterprise/server/appserver/target/rhq-server-${RHQ_VERSION}.zip gkhachik,rhqbuild@shell.sourceforge.net:/home/frs/project/rhqbuild/rhq/rhq-${RHQ_VERSION}/
